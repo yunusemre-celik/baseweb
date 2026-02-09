78994a35dbca2da2ebc2b0723d9c779536718943
yunus emre çelik <yunusemrecelikbusiness@gmail.com>
2026-02-09 20:47:22 +0300

    content:main.js

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// CSS dosyamızı import ediyoruz (Vite bunu otomatik işler ve sayfaya gömer)
+import '../css/style.css'
+
+// Konsola "Ben buradayım" mesajı (Sistemin çalıştığını test etmek için)
+console.log('BaseWeb v1 başlatıldı! 🚀')
+
+// İleride buraya interaktif kodlar gelecek (Menü açma/kapama vb.)
